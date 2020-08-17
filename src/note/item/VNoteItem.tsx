ca5b4d0ce509bd0cb9942f930a0b05e20100a62d
Henry <liaohengyi@outlook.com>
2020-08-17 12:17:56 -0400

    VNoteItem bg-white

--- src/note/item/VNoteItem.tsx
+++ src/note/item/VNoteItem.tsx
@@ -31,7 +31,7 @@ export class VNoteItem extends VNoteBase<CNoteItem> {
 			if (this.isMe(owner) === false) {
 				divFrom = this.renderFrom();
 			}
-			return <div className="d-block">
+			return <div className="d-block bg-white">
 				{caption && <div className="px-3 py-2"><b>{caption}</b></div>}
 				{this.renderContent()}
 				{

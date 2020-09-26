f078caea53e11bbf6f83efc812278abedf4f9676
bruceth <sunbaichuan@hotmail.com>
2020-09-26 21:29:31 +0800

    assignhours

--- src/notes/note/task/start/VTaskStart.tsx
+++ src/notes/note/task/start/VTaskStart.tsx
@@ -35,12 +35,12 @@ export class VTaskStart extends VTaskView<CTaskStart> {
 		return <div className="flex-fill mr-3 "><input className="flex-fill form-control border-0"
 			type="text" defaultValue={taskTimeToString(this.controller.hours)}
 			placeholder="2.5或者2：30表示两个半小时"
-			onBlur={this.onHoursBlur}
+			onBlur={e=>this.onHoursBlur(e)}
 			onChange={e=>this.onHoursChange(e)}/>
 		</div>;
 	}
 
-	protected onHoursChange(evt:React.ChangeEvent<HTMLInputElement>) {
+	protected onHoursChange = (evt:React.ChangeEvent<HTMLInputElement>) => {
 		let m = checkHourMinutes(evt.target.value);
 		if (m < 0) {
 			m = 0;
